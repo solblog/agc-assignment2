964619c12210e32caefdd04c6da022de624e8f9e
sol <solitblogdoc@gmail.com>
2020-03-02 23:59:11 +0100

    Exercise done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  userName : string; 
+
+  isEmptyUser(){
+    if (!this.userName)
+      return true; 
+  }
+
+  onClickReset(){
+    this.userName='';
+  }
+
 }
